811ac1955fc3377e0213c3f820df468abb18fa2e
AtmacaYigit <125821865+AtmacaYigit@users.noreply.github.com>
2023-03-24 18:17:11 +0300

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -11,8 +11,7 @@ def calculate_hand(hand):
     total_value = sum(card_values[card] for card in hand)
     if 'A' in hand and total_value > 21:
         total_value -= 10
-        return total_value
-
+    return total_value
 
 def deal_card():
     return deck.pop()
@@ -23,18 +22,23 @@ def play_game():
     player_hand = [deal_card(), deal_card()]
     dealer_hand = [deal_card(), deal_card()]
     
-    print("Your hand: ", player_hand, calculate_hand(player_hand))
-    print(f"Dealer's hand: ['{dealer_hand[0]}' '?'] "calculate_hand(dealer_hand)))
+    print("Your hand: ", player_hand, "total value ==>", calculate_hand(player_hand))
+    print(f"Dealer's hand: ['{dealer_hand[0]}' '?']")
 
     while True:
         action = input("Do you want to hit or stand (h or s) ? ")
         if action.lower() == 'h':
             player_hand.append(deal_card())
-            print("Your hand: ", player_hand)
-            
-            if calculate_hand(player_hand) > 21:
-                print("Total card value exceeds 21. You lose!")
-        elif action.lower() == 'stand':
+            print("Your hand: ", player_hand, "total value ==>", calculate_hand(player_hand))
+            print(f"Dealer's hand: ['{dealer_hand[0]}' '?']")
+        
+        if calculate_hand(player_hand) > 21:
+            print("Total card value exceeds 21. You lose!")
+            break
+        elif calculate_hand(player_hand) == 21:
+            print("You win!")
+        
+        elif action.lower() == 's':
             break
         else:
             print("Invalid action, please enter 'h' or 's'")
@@ -45,15 +49,18 @@ def play_game():
     
     if calculate_hand(dealer_hand) > 21:
         print("Dealer's total card value exceeds 21. You win!")
-
+    
+    elif calculate_hand(player_hand) == 21:
+        print("You lose!")
     player_total = calculate_hand(player_hand)
     dealer_total = calculate_hand(dealer_hand)
     
-    if player_total > dealer_total:
+    if 21 > player_total > dealer_total:
         print("You win!")
-    elif dealer_total > player_total:
+    elif 21 > dealer_total > player_total:
         print("Dealer won!")
-    else:
+    elif player_total == dealer_total and player_total > 17:
         print("Draw!")
+
 
 play_game()
