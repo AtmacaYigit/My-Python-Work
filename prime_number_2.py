499892cbe1acad427b9ee74700a04e332d918adb
AtmacaYigit <125821865+AtmacaYigit@users.noreply.github.com>
2025-09-17 00:30:27 +0300

    Add files via upload

---
+++ prime_number_2.py
@@ -0,0 +1,13 @@
+sayi1 = int(input("1. sayı: "))
+sayi2 = int(input("2. Sayı: "))
+
+def asalsayi(sayi1, sayi2):
+    for sayi in range(sayi1, sayi2+1):
+        if sayi > 1:
+            for i in range(2, sayi):
+                if sayi % i == 0:
+                    break
+            else:
+                print(sayi)        
+
+asalsayi(sayi1, sayi2)
